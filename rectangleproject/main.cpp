caf0ad39e70c485c3b5dac24a3729b81acb0543e
Abbie Thorne <thorneal@vcu.edu>
2019-09-05 19:05:13 -0400

    Initial Commit

---
+++ rectangleproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rectangleproject
+//
+//  Created by Abbie Thorne on 9/5/19.
+//  Copyright © 2019 Abbie Thorne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
